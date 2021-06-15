734c73dbeac4dc211da6dbaf5ad2f20bb2d5193f
chl <1326090238@qq.com>
2021-06-15 21:11:36 +0800

    去除头部

--- public/UEditor/dialogs/table/edittable.js
+++ public/UEditor/dialogs/table/edittable.js
@@ -1,10 +1,4 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: xuheng
- * Date: 12-12-19
- * Time: 下午4:55
- * To change this template use File | Settings | File Templates.
- */
+
 (function () {
     var title = $G("J_title"),
         titleCol = $G("J_titleCol"),
